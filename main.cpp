4b44992bcb3608c72129739b1c23ad0d07df5e49
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-11-20 15:22:15 +0100

    new updates

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-
 #include <SDL.h>
-#include <stdio.h>
-#include <iostream>
-#include <SDL2>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
