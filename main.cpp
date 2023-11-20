66d2bba3e1226b31a36e96d52260cc0a6b4ac5fd
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-20 18:38:26 +0100

    new updates

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* args[])
 	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		print( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else
 	{
