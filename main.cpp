68470a4041f2960ba83aaebc572eb024cd24d36f
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-11-20 15:09:32 +0100

    new updates

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
 
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 #include <iostream>
 #include <SDL2>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
