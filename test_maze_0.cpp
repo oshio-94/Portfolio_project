407fb5f852413f984566ea1e93e1945a0321622d
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-25 09:46:30 +0000

    updates

--- test_maze_0.cpp
+++ test_maze_0.cpp
@@ -13,7 +13,20 @@ Game::~Game()
 {}
 
 void Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
-{}
+{
+     int flags = 0;
+     if(fullscreen)
+     {
+	     flags = SLD_WINDOW_FULLSCREEN;
+     }
+     if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+     {
+	     std::cout << "Subsystems Initialised" << std::endl;
+
+	     window = SDL_CreateWindow(title, xpos, ypos, width, height, flags)
+	
+
+}
 
 void Game::updated()
 {}
