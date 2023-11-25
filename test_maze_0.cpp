53ab909e6288721bde74bddd03a9c1fb8c867334
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-25 11:24:31 +0000

    updates

--- test_maze_0.cpp
+++ test_maze_0.cpp
@@ -12,7 +12,7 @@ Game::Game()
 Game::~Game()
 {}
 
-void Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
      int flags = 0;
      if(fullscreen)
