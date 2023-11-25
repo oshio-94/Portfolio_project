571f94b82e0b10c040fe1f38fdd62123b23f2aeb
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-25 11:47:47 +0000

    updates

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "test_maze_0.hpp"
+
+
+Game *game = nullptr;
+
+int main(int argc, const char * arv[]) {
+	game = new Game();
+
+	game->init(Maze-Challenge, SDL_WINDOWPOS_CENTERED, SDL_WINDOWSPOS_CENTERED, 700, 500, false)
+
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+
+	return 0;
+}
