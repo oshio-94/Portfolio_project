027012dc8c7c2b7dd3d27a01702f33300b10343e
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-25 09:52:44 +0100

    new updates

---
+++ test_maze_0.cpp
@@ -0,0 +1,8 @@
+//
+//  test_maze_0.cpp
+//  Maze_Project
+//
+//  Created by Ryomen_Sukuna on 25/11/2023.
+//
+
+#include "test_maze_0.hpp"
---
+++ test_maze_0.hpp
@@ -0,0 +1,13 @@
+//
+//  test_maze_0.hpp
+//  Maze_Project
+//
+//  Created by Ryomen_Sukuna on 25/11/2023.
+//
+
+#ifndef test_maze_0_hpp
+#define test_maze_0_hpp
+
+#include <stdio.h>
+
+#endif /* test_maze_0_hpp */
