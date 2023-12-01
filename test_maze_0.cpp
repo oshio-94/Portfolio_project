e0c7f5c8f4f7df883946614317fa052ffc113ae7
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-01 10:16:44 +0100

    new files

--- test_maze_0.cpp
+++ test_maze_0.cpp
@@ -36,7 +36,7 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	     renderer = SDL_CreateRenderer(window, -1, 0);
 	     if(renderer)
 	     {
-		     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		     SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 		     std::cout << "Renderer created" << std::endl;
 	     }
 
