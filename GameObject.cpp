d4f93ec82caf9f3f53bbacd8beed31fbf801fabe
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-01 10:27:20 +0100

    new updates

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,1 +1,17 @@
-#include ""
+
+#include "GameObject.h"
+#include "textureManager.h"
+
+
+GameObject::GameObject(const char* texturesheet, SDL_Renderer* ren){
+    renderer = ren;
+    objTexture = textureManager::LoadTexture(texturesheet, ren);
+}
+
+void GameObject::Update(){
+
+}
+
+void GameObject::Render(){
+    SDL_RenderCopy(renderer, objTexture, &srcRect, &destRect);
+}
