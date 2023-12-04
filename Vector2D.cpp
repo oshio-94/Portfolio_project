f6aa21a75880f65d53f997caf7e6cd9e9a313b8d
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-04 20:36:29 +0100

    new updates

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -37,3 +37,35 @@ Vector2D& Vector2D::Divide(const Vector2D& vec){
     
     return *this;
 }
+
+Vector2D& operator+(Vector2D& v1, const Vector2D& v2){
+    return v1.Add(v2);
+}
+
+Vector2D& operator-(Vector2D& v1, const Vector2D& v2){
+    return v1.Subtract(v2);
+}
+
+Vector2D& operator*(Vector2D& v1, const Vector2D& v2){
+    return v1.Multiply(v2);
+}
+
+Vector2D& operator/(Vector2D& v1, const Vector2D& v2){
+    return v1.Divide(v2);
+}
+
+Vector2D& Vector2D::operator+=(const Vector2D& vec){
+    return this->Add(vec);
+}
+
+Vector2D& Vector2D::operator-=(const Vector2D& vec){
+    return this->Subtract(vec);
+}
+
+Vector2D& Vector2D::operator*=(const Vector2D& vec){
+    return this->Multiply(vec);
+}
+
+Vector2D& Vector2D::operator/=(const Vector2D& vec){
+    return this->Divide(vec);
+}
