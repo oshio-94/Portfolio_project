80d0938b91f77620cd6cbab65cb6d815f7864772
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-04 12:43:45 +0100

    new files

--- ECS.h
+++ ECS.h
@@ -17,8 +17,59 @@
 class Component;
 class Entity;
 
+using ComponentID = std::size_t;
 
+inline ComponentID getComponentTypeID(){
+    static Component lastID = 0;
+    return lastID++;
+}
 
+template <typename T> inline ComponentID getComponentID() noexcept{
+    static CompoxxnentID typeID = getComponentID();
+    return typeID();
+}
+
+constexpr std::size_t maxComponents = 32;
+using ComponentBitset = std::bitset<maxComponents>;
+using ComponentArray = std::array<Component*, maxComponents>;
+
+class Component{
+    public:
+        Entity* entity;
+
+        virtual void init() {}
+        virtual void update() {}
+        virtual void draw() {}
+
+        virtual ~Component() {}
+};
+
+class Entity{
+    private:
+        bool active = true;
+        std::vector<std::unique_ptr<Component>> components;
+
+        ComponentArray componentArray;
+        ComponentBitset componentBitset;
+
+    public:
+        void update() {
+            for(auto& c : components) c->update();
+            for(auto& c : components) c->draw();        
+        }
+        void draw() {}
+        bool isActive() { return active; }
+        void destroy() { active = false; }
+
+        template <typename T> bool hasComponent() const{
+            return componentBitSet[getComponentID<T>];
+        }
+
+        template <typename T, typename... TArgs>
+        T& add Component(TArgs &&... mArgs){
+            T* c(new T(std::forward<TArgs>(mArgs)...));
+        }
+};
 
 
 
