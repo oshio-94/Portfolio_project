3df64accbc871a843fa417427fbb810324a2182f
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-04 20:56:07 +0100

    new updates

--- TransformComponent.h
+++ TransformComponent.h
@@ -10,17 +10,13 @@ class TransformComponent : public Component{
 
         TransformComponent(){
             position.x = 0.0f;
-            posotion.y = 0.0f;
+            position.y = 0.0f;
         }
 
-        TransformComponent(int x, int y){
-            xpos = x;
-            ypos = y;
+        TransformComponent(float x, float y){
+            position.x = x;
+            position.y = y;
         }
-        int x() { return xpos; }
-        int y() { return ypos; }
-        void x(int x) { xpos = x; }
-        void y(int y) { ypos = y; }
 /*
         void init() override{
             xpos = 0;
@@ -30,9 +26,5 @@ class TransformComponent : public Component{
         void update() override {
             xpos++;
             ypos++;
-        }
-        void setPos(int x, int y) {
-            xpos = x;
-            ypos = y;
         }
 };
