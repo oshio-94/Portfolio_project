cd06d830ed5b7521425f06b5b19188a83ff8ea0d
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-04 20:23:27 +0100

    new updates

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -9,3 +9,31 @@ Vector2D::Vector2D(float x, float y){
     this->x = x;
     this->y = y;
 }
+
+Vector2D& Vector2D::Add(const Vector2D& vec){
+    this->x += vec.x;
+    this->y += vec.y;
+    
+    return *this;
+}
+
+Vector2D& Vector2D::Subtract(const Vector2D& vec){
+    this->x -= vec.x;
+    this->y -= vec.y;
+    
+    return *this;
+}
+
+Vector2D& Vector2D::Multiply(const Vector2D& vec){
+    this->x *= vec.x;
+    this->y *= vec.y;
+    
+    return *this;
+}
+
+Vector2D& Vector2D::Divide(const Vector2D& vec){
+    this->x /= vec.x;
+    this->y /= vec.y;
+    
+    return *this;
+}
