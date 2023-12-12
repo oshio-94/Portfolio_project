5aa75a17aeb0da5a057863b932ace53b07ed595d
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-12 11:23:30 +0100

    Deconstructor for Map textures

--- Map.cpp
+++ Map.cpp
@@ -42,6 +42,13 @@ Map::Map() {
     dest.x = dest.y = 0;
 }
 
+Map::~Map(){
+    SDL_DestroyTexture(grass);
+    SDL_DestroyTexture(water);
+    SDL_DestroyTexture(dirt);
+    SDL_DestroyTexture(wall);
+    SDL_DestroyTexture(defaut);
+}
 void Map::LoadMap(int arr[20][25]){
     for (int row = 0; row < 20; row++){
         for(int column = 0; column < 25; column++){
