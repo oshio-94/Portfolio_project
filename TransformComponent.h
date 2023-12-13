49557424317f1db027366fed25c0a754de00bfcf
John Ikhanoba <oshioi@yahoo.co.uk>
2023-12-13 10:08:33 +0100

    new updates

--- TransformComponent.h
+++ TransformComponent.h
@@ -9,8 +9,8 @@ class TransformComponent : public Component{
         Vector2D position;
         Vector2D velocity;
 
-        int height = 32;
-        int width = 32;
+        int height = 16;
+        int width = 16;
         int scale = 1;
 
         int speed = 3;
