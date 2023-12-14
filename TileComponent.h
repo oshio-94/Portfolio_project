c8cd97649269dffd4864511048a3a500ffb0c258
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-14 16:28:25 +0100

    Tile Components for Collisions

--- TileComponent.h
+++ TileComponent.h
@@ -7,11 +7,12 @@
 
 class TileComponent : public Component{
     public:
-        TransformComponent *transform;
+        TransformComponent *transform; //Give access to our position
         SpriteComponent *sprite;
 
         SDL_Rect tileRect;
         int tileID;
+        char* path;
 
         TileComponent() default;
 
@@ -21,6 +22,34 @@ class TileComponent : public Component{
             tileRect.w = w;
             tileRect.h = h;
             tileID = id;
+
+            switch (tileID){
+                case 0:
+                    path: "Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/water.png";
+                    break;
+                case 1:
+                    path = "/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/grass.png";
+                    break;
+                case 2:
+                    path = "/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/dirt.png";
+                    break; 
+                case 3:
+                    path = "/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/wall.png";
+                    break;
+                case 4:
+                    path = "/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/default.png";
+                    break;   
+            }
+
+        }
+        // initialise class
+        //add transform component to tile
+        void init() override{
+            entity->addComponent<TransformComponent>(tileRect.x, tileRect.y, tile.Rect.w, tileRect.h, 1);
+            transform = &entity->getComponent<TransformComponent>();
+
+            entity->addComponent<SpriteComponent>(path);
+            sprite = &entity->getComponent<SpriteComponent>();
 
         }
 };
