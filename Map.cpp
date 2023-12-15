cccac89e31136172824aa13fc76727ae8e68e234
Oshio Ikhanoba <oshioi@yahoo.co.uk>
2023-12-15 15:39:23 +0100

    tile map to game

--- Map.cpp
+++ Map.cpp
@@ -1,63 +1,33 @@
 #include "Map.h"
-#include "textureManager.h"
-
+#include "test_maze_0.hpp"
+#include <fstream>
 //Contains all the components for the map functions
-int lvl1[20][25] = {
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1,1},
-    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1,1},
-    {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
+
 
 Map::Map() {
-    dirt = textureManager::LoadTexture("/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/dirt.png");
-    grass = textureManager::LoadTexture("/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/grass.png");
-    water = textureManager::LoadTexture("/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/water.png");
-    wall = textureManager::LoadTexture("/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/wall.png");
-    defaut = textureManager::LoadTexture("/Users/emike/Documents/Maze_Project/Maze_Project/Maze_Project/assets/default.png");
-  
 
-    LoadMap(lvl1);
-
-    src.x = src.y = 0;
-    src.w = dest.w = 32;
-    src.h = dest.h = 32;
-
-    dest.x = dest.y = 0;
 }
 
 Map::~Map(){
-    SDL_DestroyTexture(grass);
-    SDL_DestroyTexture(water);
-    SDL_DestroyTexture(dirt);
-    SDL_DestroyTexture(wall);
-    SDL_DestroyTexture(defaut);
+
 }
-void Map::LoadMap(int arr[20][25]){
-    for (int row = 0; row < 20; row++){
-        for(int column = 0; column < 25; column++){
-            map[row][column] = arr[row][column];
+void Map::LoadMap(std::string path, int sizeX, int sizeY){
+    char tile;
+    std::fstream mapFile;
+    mapFile.open(path);
+
+    for(int y = 0; y < sizeY; y++){
+        for(int x = 0; x < sizeX; x++){
+            mapFile.get(tile);
+            Game::AddTile(atoi(&tile), x * 32, y * 32);
+            map.File.ignore();
         }
     }
+
+    mapFile.close();
 }
 
-void Map::DrawMap(){
+/* oid Map::DrawMap(){
     int type = 0;
     for (int row = 0; row < 20; row++){
         for(int column = 0; column < 25; column++){
@@ -88,4 +58,4 @@ void Map::DrawMap(){
             }
         }
     }
-}
+} */
--- Map.h
+++ Map.h
@@ -1,18 +1,18 @@
 #pragma once
-#include "test_maze_0.hpp"
+//#include "test_maze_0.hpp"
 
 class Map {
     public:
         Map();
         ~Map();
 
-        void LoadMap(int arr[20][25]);
-        void DrawMap();
+        void LoadMap(std::string path, int sizeX, int sizeY);
+        //void DrawMap();
 
 
     private:
         
-        SDL_Rect src, dest;
+        /*SDL_Rect src, dest;
 
         SDL_Texture* dirt;
         SDL_Texture* grass;
@@ -20,5 +20,5 @@ class Map {
         SDL_Texture* wall;
         SDL_Texture* defaut;
     
-        int map[20][25];
+        int map[20][25]; */
 };
